6081d7f4ae374a1765a391c873bc8071b5a18a5c
mixelpixel <mixelpix@mac.com>
2017-02-19 18:53:12 -0500

    leson 3

--- lesson3/lambdablog/blog.py
+++ lesson3/lambdablog/blog.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
+# 'render_template' renders templates and 'request' handles http requests
+
 import sqlite3
+
 app = Flask(__name__)
 
+#connecting application to the database
 connection = sqlite3.connect('database.db')
 print('Database opened successfully')
 
@@ -13,6 +17,10 @@ connection.close()
 def hello_world():
     return "Hello World!"
 
+# Rendering a template:
 @app.route('/new')
 def new_post():
     return render_template('new.hmtl')
+
+if __name__ == '__main__':
+    app.run(debug = True)
