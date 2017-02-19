1f3e0fd92132953f01cef94032ce10f8835b4d2e
mixelpixel <mixelpix@mac.com>
2017-02-19 16:53:19 -0500

    subtraction and multiplication returns

--- lesson2/homework/homework_website.py
+++ lesson2/homework/homework_website.py
@@ -21,5 +21,13 @@ def greeting(name):
 def add(num1, num2):
     return str(num1 + num2)
 
+@app.route('/subtract/<int:num1>/<int:num2>')
+def subtract(num1, num2):
+    return str(num1 - num2)
+
+@app.route('/multiply/<int:num1>/<int:num2>')
+def multiply(num1, num2):
+    return str(num1 * num2)
+
 if __name__ == '__main__':
     app.run(debug = True)
