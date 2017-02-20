d918eb6aa81a915d32393170adef16fdfb2454e5
mixelpixel <mixelpix@mac.com>
2017-02-19 23:41:42 -0500

    struggling

--- lesson3/homework/server.py
+++ lesson3/homework/server.py
@@ -13,17 +13,20 @@ def home():
 def enternew():
     return render_template('food.html')
 
-@app.route('/addfood', methods = {'POST'})
+@app.route('/addfood', methods = ['POST'])
 def addfood():
     connection = sqlite3.connect('database.db')
     cursor = connection.cursor()
     try:
-        name = request.form['name']
-        calories = request.form['calories']
-        cuisine = request.form['cuisine']
-        is_vegetarian = request.form['is_vegetarian']
+        name           = request.form['name']
+        calories       = request.form['calories']
+        cuisine        = request.form['cuisine']
+        is_vegetarian  = request.form['is_vegetarian']
         is_gluten_free = request.form['is_gluten_free']
-        cursor.execute('INSERT INTO foods (name,calories,cuisine,is_vegetarian,is_gluten_free) VALUES (?,?,?,?,?)',(name,calories,cuisine,is_vegetarian,is_gluten_free))
+        cursor.execute('INSERT INTO foods (name,calories,cuisine,\
+                                           is_vegetarian,is_gluten_free) \
+                        VALUES (?,?,?,?,?)',(name,calories,cuisine,\
+                                             is_vegetarian,is_gluten_free))
         connection.commit()
         message = "Yum! Food successfuly added"
     except:
